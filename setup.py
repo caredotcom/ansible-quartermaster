1b452bb2ebbd296bfa34508f266426df5a4c9a38
Bruce Pennypacker <bpennypacker@care.com>
2016-08-23 18:14:24 +0000

    Converted quartermaster into a module

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='ansible_quartermaster', 
+      version='1.0',
+      description='Care.com Ansible Quartermaster module',
+      url='https://github.com/caredotcom/ansible-quartermaster',
+      py_modules=['ansible_quartermaster']
+     )
+
